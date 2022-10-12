114099366ea13a40d27df2c166ce3a73d03b9aa2
bofmar <bofmar@outlook.com>
2022-10-12 13:59:34 +0300

    Setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -12,6 +12,14 @@ module.exports = {
         test: /\.scss$/i,
         use: ['style-loader', 'css-laoder', 'sass-loader'],
       },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
