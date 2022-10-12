05c74f3676c8e7222ac34a11c2900ee6c90eff1a
bofmar <bofmar@outlook.com>
2022-10-12 17:40:36 +0300

    make icons apear on the board for knight and goal

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import drawBoard from "./modules/drawBoard";
 import signature from "./modules/signature";
 import placeKnight from "./modules/placeKnight";
 import placeGoal from "./modules/placeGoal";
+
+import Knight from './assets/knight.svg';
+import Goal from './assets/goal.svg';
 import './styles/index.scss';
 
 const board = document.getElementById('board-wrapper');
@@ -15,14 +18,24 @@ let goalPosition: number;
   placeKnight();
   placeGoal();
 
+  // create the image for the knight
+  const kimg = new Image();
+  kimg.src = Knight;
+
+  // create the image for the goal
+  const gimg = new Image();
+  gimg.src = Goal;
+
   // get the id of the clicked square and set it as the new position of either the knight or the goal.
   Array.from(board.children).forEach(square => {
     square.addEventListener('click', () => {
       if (!board.classList.contains('placing')) return;
       if (board.classList.contains('placing-knight')) {
         knightPosition = parseInt(square.id);
+        square.appendChild(kimg);
       } else {
         goalPosition = parseInt(square.id);
+        square.appendChild(gimg);
       }
       board.className = '';
     });
